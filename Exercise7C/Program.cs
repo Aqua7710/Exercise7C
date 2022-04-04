f9a09732bcbade31fdd020c6529b14b671bbd514
William Purdon <ac117097@avcol.school.nz>
2022-04-05 11:11:51 +1200

    Add project files.

---
+++ Exercise7C/Program.cs
@@ -0,0 +1,8 @@
+﻿double sideA, sideB, sideC;
+Console.WriteLine("Hello user! This program will ask for the lengths of three sides of a triangle as inputs and then it will \ncalculate whether or not the triangle is a right angled triangle.");
+Console.Write("\nLegth A = ");
+sideA = Convert.ToDouble(Console.ReadLine());
+Console.Write("\nLength B = ");
+sideB = Convert.ToDouble(Console.ReadLine());
+Console.Write("\nLength C = ");
+sideC = Convert.ToDouble(Console.ReadLine());
